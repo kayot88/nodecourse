09007afac2c0c630a8bcb57c9367e388370f2943
Kayot88 <kayot88@gmail.com>
2018-10-22 09:21:11 +0300

    22.10/ 09.20

--- 4/Samples/http/001_http.js
+++ 4/Samples/http/001_http.js
@@ -6,14 +6,11 @@ var server = http.Server();
 server.on('request', function(request, response){
     console.log('Connection established!');    
     console.log(request.method);
-
-    // Завершает конфигурацию ответ
-    response.end();
-    // response.end("Responce is configured!", 'utf8', () => { console.log('finish'); });
+    response.end("Responce is configured!", 'utf8', () => { console.log('finish'); });
 });
 
+// server.listen(8080)
 server.listen(8080)
-
 server.on('listening', function(){
     console.log('Server running on port 8080');
 });
--- 4/Samples/http/002_events.js
+++ 4/Samples/http/002_events.js
@@ -1,13 +1,20 @@
-var http = require("http");
+// var http = require("http");
 
-var server = http.createServer(function(request, response) {
-    // writeHead - метод позволяет записать в ответ заголовки и статус код
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    // write - метод позволяет создать тело ответа в виде потока writeable Stream
-    response.write("Hello World");
-    // end - завершает конфигурациюответа и отправляет его
-    response.end();
+// var server = http.createServer(function(request, response) {
+//     response.writeHead(300, {"Content-Type": "text/plain"});
+//     response.write("Hello Worlddd");
+//     response.end();
 
-}).listen(8080, function(){
-    console.log('Server running on port 8080');
-});
+// }).listen(8080, function(){
+//     console.log('Server running on port 8080');
+// });
+
+let http = require('http');
+let server = http.createServer((request, response) => {
+    
+}).listen(8080, () => {
+    console.log(`Server started on 8080`);
+})
+
+
+
